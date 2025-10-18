89e94d77b6c40e536d3a63404c82611dcbfbed4f
ManuelMarmol2710 <92897105+ManuelMarmol2710@users.noreply.github.com>
2025-10-18 17:29:46 -0400

    clean effects

--- Effects/drag-press/grab.ts
+++
@@ -1,9 +0,0 @@
-export const grab = {
-  start: () => document.body.classList.add("gesture-grabbing"),
-  end: () => document.body.classList.remove("gesture-grabbing"),
-};
-
-export const resize = {
-  start: () => document.body.classList.add("gesture-resizing"),
-  end: () => document.body.classList.remove("gesture-resizing"),
-};
